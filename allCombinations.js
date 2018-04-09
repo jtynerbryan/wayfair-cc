43484cacc24bce95d6b025843e48c115a02eb8e1
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-04-09 12:14:19 -0400

    updated with comments

--- allCombinations.js
+++ allCombinations.js
@@ -1,17 +1,26 @@
-const array = [['quick', 'lazy'], ['brown', 'black', 'grey'], ['fox', 'dog']]
+const array = [['quick', 'lazy'], ['brown', 'black', 'grey'], ['fox', 'dog']];
 
 const allCombinations = (array) => {
+  // iterate over array of arrays
   return array.reduce((a, b) => {
-    let result = []
-    a.forEach(x => {
-      b.forEach(y => {
-        result.push([].concat(x, y))
-      })
-    })
-    return result
-  })
-}
+    // create empty array that will become the accumulator (the value of a for each iteration)
+    let result = [];
+    // iterate over the value of a (first time around it will be ['quick', 'lazy'])
+    a.forEach((x) => {
+      // iterate over b (first time around will be ['brown', 'black', 'grey'])
+      b.forEach((y) => {
+        // push a new array into result, combining each element in a with all elements in b
+        // first array pushed to result will be ['quick', 'brown'], next will be ['quick', 'black'], then ['quick', 'grey'] etc.
+        result.push([].concat(x, y));
+      });
+    });
+    // result is now a 2 dimensional array with all combos of a and b
+    // return result so it becomes the new value of a for the next iteration
+    return result;
+    // for the second iteration the value of b will now be ['fox', 'dog']
+    // repeat the same process pushing new combos to re-initialized result array
+    // ex. result.push([].concat(['quick', 'brown'], 'fox')), then result.push([].concat(['quick', 'brown'], 'dog')) etc.
+  });
+};
 
-const result = allCombinations(array)
-
-console.log(result)
+allCombinations(array);
