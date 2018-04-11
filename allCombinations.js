e3d49f1b585d27ba55ea2ccc79c5e83516f06ede
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-04-11 11:28:06 -0400

    added a more functional approach

--- allCombinations.js
+++ allCombinations.js
@@ -1,14 +1,14 @@
 const array = [['quick', 'lazy'], ['brown', 'black', 'grey'], ['fox', 'dog']];
 
-const allCombinations = (array) => {
+const allCombinations = array => {
   // iterate over array of arrays
   return array.reduce((a, b) => {
     // create empty array that will become the accumulator (the value of a for each iteration)
     let result = [];
     // iterate over the value of a (first time around it will be ['quick', 'lazy'])
-    a.forEach((x) => {
+    a.forEach(x => {
       // iterate over b (first time around will be ['brown', 'black', 'grey'])
-      b.forEach((y) => {
+      b.forEach(y => {
         // push a new array into result, combining each element in a with all elements in b
         // first array pushed to result will be ['quick', 'brown'], next will be ['quick', 'black'], then ['quick', 'grey'] etc.
         result.push([].concat(x, y));
@@ -23,4 +23,13 @@ const allCombinations = (array) => {
   });
 };
 
-allCombinations(array);
+// allCombinations(array);
+
+const allCombinationsOneLine = array => array.reduce((a, b) => [].concat(...a.map(x => b.map(y => [].concat(x, y)))));
+// This new version has the same time complexity, but uses .map instead of .forEach.
+// This is a more functional approach because .map, unlike .forEach, doesn't rely on side effects to function properly
+console.log(allCombinationsOneLine(array));
+
+// time complexity = O(n^3)
+// We have .reduce which is O(n)
+// two nested .map loops which are each O(n)
